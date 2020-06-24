66cddf15dca78de6bb9b3ab4b6e899c557587c0a
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-06-24 18:45:52 +0200

    Only use TF v2.0.0 for OS X & Conda, otherwise v2.2.0

--- ci/travis-install-conda.sh
+++ ci/travis-install-conda.sh
@@ -4,7 +4,6 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-${conda_os}-x86_64.sh
 bash ~/miniconda.sh -b -p ~/miniconda
 source ~/miniconda/bin/activate
 
-# TF 2.2.0 is not available on conda for OS X yet, let's stick to 2.0.0 for both OS X and Linux
-conda install --yes --quiet --channel=conda-forge tensorflow=2.0.0
+conda install --yes --quiet --channel=conda-forge tensorflow=${conda_tf_version}
 
 set +e +x
