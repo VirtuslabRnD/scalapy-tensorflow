6c29df2789f7d73e75043173f3c8b63d78fa409b
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-06-24 16:18:55 +0200

    Skip standalone keras installation

--- ci/travis-install-conda.sh
+++ ci/travis-install-conda.sh
@@ -5,6 +5,6 @@ bash ~/miniconda.sh -b -p ~/miniconda
 source ~/miniconda/bin/activate
 
 # TF 2.2.0 is not available on conda for OS X yet, let's stick to 2.0.0 for both OS X and Linux
-conda install --yes --quiet --channel=conda-forge tensorflow=2.0.0 keras
+conda install --yes --quiet --channel=conda-forge tensorflow=2.0.0
 
 set +e +x
--- ci/travis-install-pip.sh
+++ ci/travis-install-pip.sh
@@ -4,6 +4,5 @@ pip config set global.progress_bar off
 pip install --upgrade pip
 pip install numpy
 pip install tensorflow==2.2.0
-pip install keras
 
 set +e +x
