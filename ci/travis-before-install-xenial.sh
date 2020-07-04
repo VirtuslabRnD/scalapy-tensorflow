6e27a906e3ebe256a2aec6aaedd433f42d4d5f49
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-07-04 21:19:10 +0200

    Remove liblzma-dev from Xenial setup

--- ci/travis-before-install-xenial.sh
+++ ci/travis-before-install-xenial.sh
@@ -8,6 +8,6 @@ export PATH=/usr/local/clang-5.0.0/bin:$PATH
 export CXX=clang++
 
 sudo apt-get update
-sudo apt-get install -y liblzma-dev libre2-dev libunwind-dev
+sudo apt-get install -y libre2-dev libunwind-dev
 
 set +e +x
