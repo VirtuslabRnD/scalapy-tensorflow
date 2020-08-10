052caf2ef5a291a4fdf2eada68462395abfe0f9b
Bartek <bartektonia@gmail.com>
2020-08-10 11:28:52 +0200

    old numpy (#48)
    * Prevent double-linking of OpenMP on OS X/Conda setup
    
    * old numpy
    
    * no mkl test
    
    * add comment
    
    Co-authored-by: Pawel Lipski <pawel.p.lipski@gmail.com>

--- ci/travis-install-conda.sh
+++ ci/travis-install-conda.sh
@@ -6,5 +6,7 @@ source ~/miniconda/bin/activate
 
 conda install --yes --quiet python=3.7
 conda install --yes --quiet tensorflow=${conda_tf_version}
-
+# There is a difference in versions of BLAS library between numpy 1.18 and 1.19. 
+# It caused segfaults so numpy is kept on 1.18 for now. 
+conda install --yes --quiet numpy=1.18.5
 set +e +u
